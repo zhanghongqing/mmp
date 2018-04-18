6229aff4b48482e22793c9c43c38c29e44323793
zhanghq <8366783@qq.com>
2018-04-18 08:30:52 +0800

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,4 @@
+//var viewport = document.querySelectorAll('meta[name="viewport"]')[0]
+//viewport.setAttribute('content', 'width=750,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no')
+//alert(window.innerWidth)
+//alert(window.devicePixelRatio)
